1e6afd994f400cc9478aaf7d95eacbf8e9fe86e5
Caleb Ford <caleb.ford.sto@gmail.com>
2024-10-01 14:17:47 -0500

    working commit

--- tests/order.py
+++ tests/order.py
@@ -132,3 +132,50 @@ class OrderTests(APITestCase):
         )
 
     # TODO: New line item is not added to closed order
+    def test_add_to_cart_not_closed_order(self):
+        # Create order by adding product to cart
+        url = "/cart"
+        data = {"product_id": 1}
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+        response = self.client.post(url, data, format="json")
+
+        # Get cart and verify product was added
+        url = "/cart"
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+        response = self.client.get(url, None, format="json")
+        json_response = json.loads(response.content)
+        order_id_to_close = json_response["id"]
+
+        # Send request to create payment type
+        url = "/payment-types"
+        data = {
+            "merchant": "Visa",
+            "acctNumber": 3212344556778654,
+            "expirationDate": "2027-02-01",
+        }
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+        response = self.client.post(url, data, format="json")
+        json_response = json.loads(response.content)
+        # Then get the id of the newly created payment type
+        payment_id = json_response["id"]
+
+        # Then send PUT to add payment to order to close it out
+        url = f"/orders/{order_id_to_close}"
+        data = {"paymentTypeId": payment_id}
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+        response = self.client.put(url, data, format="json")
+
+        # then add another product to cart
+        url = "/cart"
+        data = {"product_id": 1}
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+        response = self.client.post(url, data, format="json")
+        # then get new order and get it's id
+        url = "/cart"
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+        response = self.client.get(url, None, format="json")
+        json_response = json.loads(response.content)
+        order_id = json_response["id"]
+
+        # then assert that the second order's id does not equal the closed order's id
+        self.assertNotEqual(order_id_to_close, order_id)
