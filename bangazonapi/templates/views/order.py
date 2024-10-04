fab26c2b2001c2d0dba0930a26dab64ab33a50fa
Abdul Alsafri <abdulalsafri1@gmail.com>
2024-10-04 13:27:56 -0500

    report merger and acquasition.

--- bangazonapi/templates/views/order.py
+++ bangazonapi/templates/views/order.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from ...models import Order
 
 
-def unpaid_orders_report(request):
+def orders_report(request):
     status = request.GET.get("status")
     if status == "incomplete":
         orders = Order.objects.filter(payment_type=None)
@@ -24,3 +24,16 @@ def unpaid_orders_report(request):
             request, "reports/unpaid_order_report.html", {"report_data": report_data}
         )
     # Handle other cases or return an error
+    else:
+        orders = Order.objects.filter(payment_type__isnull=False)
+
+        report_data = []
+        for order in orders:
+            report_data.append({
+                'order_id': order.id,
+                'customer_name': f"{order.customer.user.first_name} {order.customer.user.last_name}",
+                'total': sum(item.product.price for item in order.lineitems.all()),
+                'payment_type': order.payment_type.merchant_name,
+            })
+
+        return render(request, 'reports/complete_orders.html', {'orders': report_data})
